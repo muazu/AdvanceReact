b573e336ccf37712c5ce1279762c7191e9af93b3
gypsyBelly <gypsybelly@gypsyBellys-MacBook-Pro.local>
2018-11-13 15:52:18 -0800

    Lesson 8

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import Link from 'next/link';
 import styled from 'styled-components';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import Nav from './Nav';
+
+Router.onRouteChangeStart = () => {
+    NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+    NProgress.done();
+};
+Router.onRouteChangeError = () => {
+    NProgress.done();
+};
 
 const Header = () => {
     return (
