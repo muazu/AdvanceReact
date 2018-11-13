830029510d1511a710422c7c7b1e5b34c2a43141
gypsyBelly <gypsybelly@gypsyBellys-MacBook-Pro.local>
2018-11-13 14:34:37 -0800

    Lesson 4

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import Link from 'next/link';
 
 const sell = () => {
     return (
         <div>
             <p>Sell!</p>
-            <Link href="/">
-                <a>Home</a>
-            </Link>
         </div>
     );
 };
